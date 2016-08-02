50c47a081580fdcf22f779413f32264c6f1d3166
Nicolas Sigal <nicolas.sigal@globallogic.com>
2016-08-02 12:34:07 -0300

    asd

--- service-worker.js
+++ service-worker.js
@@ -13,7 +13,7 @@ self.addEventListener('install', function(event) {
   '/app/routes/home/home.js',
   '/app/routes/bought/bought.html',
   '/app/routes/bought/bought.js',
-  '/app/styles/styles.css'
+  '/app/styles/styles.css',
   '/libs/font-awesome/font-awesome.min.css',
   '/libs/materialize/materialize.min.css',
   '/libs/materialize/materialize.min.js',
